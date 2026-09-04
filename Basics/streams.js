c757e62521e077086dd0c848d6447eba6e7246e0
Nitin0264 <nc8224140@gmail.com>
2026-09-04 13:12:40 +0530

    pipe

--- Basics/streams.js
+++ Basics/streams.js
@@ -2,10 +2,12 @@ const fs =require('fs')
 const readStream = fs.createReadStream('./docs/blog3.txt',{encoading:'utf8'})
 
 const writeStream = fs.createWriteStream('./docs/blog4.txt')
-readStream.on('data',(chunk)=>
-{
-  console.log('-------new chunk---------------')
-  console.log(chunk);
-  writeStream('\n New Chunk\n')
-  writeStream.write()
-})
+// readStream.on('data',(chunk)=>
+// {
+//   console.log('-------new chunk---------------')
+//   console.log(chunk);
+//   writeStream('\n New Chunk\n')
+//   writeStream.write(chunk)
+// })
+
+readStream.pipe(writeStream);
