90b95e7967286024829aea70490f11f189f40e14
Nitin0264 <nc8224140@gmail.com>
2026-09-07 00:05:49 +0530

    sharong the my first plain text and verifyin it with the network section

--- lesson3/server.js
+++ lesson3/server.js
@@ -2,7 +2,12 @@ const http = require('http')
 
 const server = http.createServer((req,res)=>
 {
- console.log(req)
+ console.log(req.url,req.method)
+
+ //set header content type 
+ res.setHeader('content-Type', 'text/plain')
+ res.write('hello ,ninjas');
+ res.end();
 });
 
 server.listen(3000,'localhost',()=>
