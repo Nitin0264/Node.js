090cc9349f60e4525bf4d04384e89888450bf31a
Nitin0264 <nc8224140@gmail.com>
2026-09-10 20:23:01 +0530

    returning into html

--- lesson3/server.js
+++ lesson3/server.js
@@ -1,15 +1,25 @@
 const http = require('http')
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>
 {
  console.log(req.url,req.method)
 
  //set header content type 
- res.setHeader('content-Type', 'text/html')
- res.write('<head>  <link rel ="stylesheet href ="#"> </head>')
- res.write(' <p>hello ,ninjas</p>');
- res.write(' <p>hello again ,ninjas</p>');
- res.end();
+ res.setHeader('content-Type', 'text/html');
+
+   fs.readFile('./views/index.html', (err,data)=>
+  {
+    if(err)
+    {
+      console.log(err);
+      res.end();
+    }
+    else{
+   
+      res.end(data);
+    }
+  })
 });
 
 server.listen(3000,'localhost',()=>
